1aae2c61038d046348b7d0b3ad47bc9b701fcbaa
zykxs <af48348@gmail.com>
2023-11-02 10:01:09 +0000

    Script Update

--- mkScript.sh
+++ mkScript.sh
@@ -1,10 +1,20 @@
+#!/bin/bash
+
 #./mkScript.sh
+
+# Check if filename is provided
+if [ -z "$1" ]; then
+    echo "Usage: $0 <filename>"
+    exit 1
+fi
 
 #Set filename
 touch $1.sh
 
 #set execute perms
-chmod -x $1.sh
+chmod +x $1.sh
 
-#Set first line in new script
+#Set first lines in new script
+echo "#!/bin/bash" >> $1.sh
+echo >> $1.sh
 echo "#./$1.sh" >> $1.sh
